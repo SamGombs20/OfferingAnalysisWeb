9cd2e96564856fb66ca541ad98f5b302e4f2020f
SamGombs20 <gombajosh18@gmail.com>
2026-03-06 13:25:45 +0300

    Update:Defining the user schema using zod

--- app/schemas/user.ts
+++ app/schemas/user.ts
@@ -1,6 +1,19 @@
 import {z} from 'zod';
 
 export const UserSchema = z.object({
+    id:z.string(),
     firstName:z.string().min(2),
-    lastName:z.string().min(2)
+    lastName:z.string().min(2),
+    church:z.string().min(2),
+    phoneNumber:z.string().min(10),
+    email:z.email(),
+    username:z.string().min(4),
+    role:z.string().default("user"),
 })
+
+export const UserLoginSchema = z.object({
+    username:z.string().min(4),
+    password:z.string().min(6)
+})
+
+export const UserRegisterSchema = UserSchema.omit({})
