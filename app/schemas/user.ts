ff80fbe2e03733c1a5119018357d935674000e80
SamGombs20 <gombajosh18@gmail.com>
2026-03-06 13:33:17 +0300

    Update:Defining the user schema using zod

--- app/schemas/user.ts
+++ app/schemas/user.ts
@@ -2,18 +2,20 @@ import {z} from 'zod';
 
 export const UserSchema = z.object({
     id:z.string(),
-    firstName:z.string().min(2),
-    lastName:z.string().min(2),
-    church:z.string().min(2),
-    phoneNumber:z.string().min(10),
-    email:z.email(),
-    username:z.string().min(4),
+    firstName:z.string().trim().min(2, "First name must be at least 2 characters!"),
+    lastName:z.string().trim().min(2, "Last name must be a t least 2 characters!"),
+    church:z.string().trim().min(2, "Church name must be at least 2 characters!"),
+    phoneNumber:z.string().trim().regex(/^\d{10}$/).length(10, "Phone number must be 10 characters long"),
+    email:z.string().email().toLowerCase(),
+    username:z.string().trim().regex(/^[a-zA-Z0-9_]+$/).min(4, "Username must be at least 4 characters!"),
     role:z.string().default("user"),
 })
 
 export const UserLoginSchema = z.object({
-    username:z.string().min(4),
-    password:z.string().min(6)
+    username:z.string().trim().regex(/^[a-zA-Z0-9_]+$/).min(4, "Username must be at least 4 characters!"),
+    password:z.string().min(6, "Password mut be at least 6 characters!")
 })
 
-export const UserRegisterSchema = UserSchema.omit({})
+export const UserRegisterSchema = UserSchema.omit({id:true}).extend({
+    password:z.string().min(6)
+})
