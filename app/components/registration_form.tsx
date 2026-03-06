6f1df9e0fdbb900572afd3e7b26df2ac6acd827f
SamGombs20 <gombajosh18@gmail.com>
2026-03-06 14:53:20 +0300

    Update:Registration form UI dev

--- app/components/registration_form.tsx
+++ app/components/registration_form.tsx
@@ -3,10 +3,14 @@ export const RegistrationForm =()=>{
         <div className="flex flex-col mt-8 w-full min-h-96 p-4 rounded-xl shadow-xl border-t border-blue-700">
             
             <div className="text-center">
-                <p className="title-text mb-1 text-3">Sign Up</p>
+                <p className="mb-1 text-2xl font-semibold">Sign Up</p>
                 <p className="italic">Create an account and start making analysis</p>
             </div>
-
+            <form>
+                <div className="flex flex-col">
+                    <input type="text" placeholder="First name" />
+                </div>
+            </form>
         </div>
     );
 }
