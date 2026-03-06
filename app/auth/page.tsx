73982f635d6881b0d0dceddc7db401e3d22de617
SamGombs20 <gombajosh18@gmail.com>
2026-03-06 14:14:47 +0300

    Update:Registration form UI dev

---
+++ app/auth/page.tsx
@@ -0,0 +1,13 @@
+import { RegistrationForm } from "../components/registration_form"
+
+const AuthPage =()=>{
+    return (
+        <div className="container w-1/2 mx-auto min-h-screen items-center flex flex-col justify-center">
+            <div>
+                <p className="text-4xl">Offering Analysis</p>
+            </div>
+            <RegistrationForm/>
+        </div>
+    )
+}
+export default AuthPage;
---
+++ app/components/registration_form.tsx
@@ -0,0 +1,11 @@
+export const RegistrationForm =()=>{
+    return (
+        <div className="flex flex-col bg-blue-500 mt-8 w-full min-h-96 p-4">
+            
+            <div className="">
+                <p>Registration Form</p>
+            </div>
+
+        </div>
+    );
+}
