259076a05dda098275852cb93a31212293189104
SamGombs20 <gombajosh18@gmail.com>
2026-03-06 14:46:03 +0300

    Update:Registration form UI dev

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -3,8 +3,9 @@ import { RegistrationForm } from "../components/registration_form"
 const AuthPage =()=>{
     return (
         <div className="container w-1/2 mx-auto min-h-screen items-center flex flex-col justify-center">
-            <div>
-                <p className="text-4xl font-bold blue-txt">Offering Analysis</p>
+            <div className="text-4xl font-bold flex">
+                <p className="blue-txt">Offering</p>
+                <p className="red-txt">Analysis</p>
             </div>
             <RegistrationForm/>
         </div>
--- app/components/registration_form.tsx
+++ app/components/registration_form.tsx
@@ -3,8 +3,8 @@ export const RegistrationForm =()=>{
         <div className="flex flex-col mt-8 w-full min-h-96 p-4 rounded-xl shadow-xl border-t border-blue-700">
             
             <div className="text-center">
-                <p className="title-text mb-1">Sign Up</p>
-                <p>Create an account and start making analysis</p>
+                <p className="title-text mb-1 text-3">Sign Up</p>
+                <p className="italic">Create an account and start making analysis</p>
             </div>
 
         </div>
