0198d0793b4af37f0288d18a0dd425e3cc29fa17
SamGombs20 <gombajosh18@gmail.com>
2026-03-06 15:17:27 +0300

    Update:Registration form UI dev

--- app/components/registration_form.tsx
+++ app/components/registration_form.tsx
@@ -11,10 +11,11 @@ export const RegistrationForm =()=>{
                     <input type="text" placeholder="First name" className="user-input"/>
                     <input type="text" placeholder="Last name" name="lastName" className="user-input" />
                     <input type="text" placeholder="Church name" name="church" className="user-input"/>
-                    <input type="number" name="phoneNumber" placeholder="Phone number" className="user-input"/>
+                    <input type="text" name="phoneNumber" placeholder="Phone number" className="user-input"/>
                     <input type="email" name="email" placeholder="Email" className="user-input"/>
                     <input type="text" name="username" placeholder="Username" className="user-input"/>
-                    <input type="text" placeholder="Password" name="password" className="user-input"/>                     
+                    <input type="password" placeholder="Password" name="password" className="user-input"/>
+                    <input type="password" name="confirmPassword" placeholder="Confirm Password" className="user-input" />                     <button className="">Register</button>
                 </div>
             </form>
         </div>
