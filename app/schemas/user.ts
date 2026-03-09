a022e53f6010b209269bd7b077e22c1a7f94fc81
SamGombs20 <gombajosh18@gmail.com>
2026-03-09 15:03:09 +0300

    Update:Registration Form development

--- app/schemas/user.ts
+++ app/schemas/user.ts
@@ -5,7 +5,7 @@ export const UserSchema = z.object({
     firstName:z.string().trim().min(2, "First name must be at least 2 characters!"),
     lastName:z.string().trim().min(2, "Last name must be a t least 2 characters!"),
     church:z.string().trim().min(2, "Church name must be at least 2 characters!"),
-    phoneNumber:z.string().trim().length(10, "Phone number must be 10 characters long").regex(/^\d{10}$/, "Phone number must contain only digits!"),
+    phoneNumber:z.string().trim().length(10, "Phone number must be 10 characters long").regex(/^\d{10}$/, "Phone number must contain only digits!").startsWith("0", "Phone number must start with 0!"),
     email:z.string().email().toLowerCase(),
     username:z.string().trim().min(4, "Username must be at least 4 characters!"),
     role:z.string(),
