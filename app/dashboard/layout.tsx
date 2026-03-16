7fa4ced99f5ab2ffd636ac6ff30945c2ba96604d
SamGombs20 <gombajosh18@gmail.com>
2026-03-16 10:18:08 +0300

    Update:User dashboard design

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,10 @@
+import { ReactNode } from "react";
+
+const DashboardLayout =({children}:{children:ReactNode})=>{
+    return(
+        <div className="flex min-h-screen">
+
+        </div>
+    )
+}
+export default DashboardLayout;
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,8 +3,10 @@ import { useAuthStore } from "../lib/store";
 
 const DashboardPage=()=>{
     const {user, logOut} = useAuthStore()
+
     return(
         <div>
+            
             <p>Welcome {user?.firstName}</p>
             <button onClick={logOut}>LogOut</button>
         </div>
