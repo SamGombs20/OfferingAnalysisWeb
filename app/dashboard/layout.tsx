7c0f0043b6af948e3f87e214cf91178aca94f8ed
SamGombs20 <gombajosh18@gmail.com>
2026-03-17 10:55:54 +0300

    Update:User dashboard design

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -5,7 +5,7 @@ const DashboardLayout = ({ children }: { children: ReactNode }) => {
   return (
     <div className="flex min-h-screen">
       <Sidebar />
-      <main className={`lg:ml-10 transition-all duration-300 ease-in-out border-l border-blue-200 px-3 pt-15`}>
+      <main className={`lg:ml-1 transition-all duration-300 ease-in-out border-l border-blue-200 px-3 pt-15`}>
         <div className="flex font-bold text-4xl ">
                 <p className="blue-txt">Offering</p>
                 <p className="red-txt">Analysis</p>
