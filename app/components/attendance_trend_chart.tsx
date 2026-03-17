57496884354872e5c11b0a4e505e79977d881398
SamGombs20 <gombajosh18@gmail.com>
2026-03-17 13:52:47 +0300

    Update:User dashboard trend charts

--- app/components/attendance_trend_chart.tsx
+++ app/components/attendance_trend_chart.tsx
@@ -1,23 +1,60 @@
-'use client'
-import {CartesianGrid, Line, LineChart, XAxis, YAxis} from 'recharts'
-import { weeklySummary } from '../data/data';
-const AttendanceChart =()=>{
-    return (
-        <LineChart
-        style={{width:'100%',maxWidth: '700px', maxHeight: '70vh', aspectRatio: 1.618}}
+"use client";
+import {
+  CartesianGrid,
+  Legend,
+  Line,
+  LineChart,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
+import { weeklySummary } from "../data/data";
+const AttendanceChart = () => {
+  return (
+    <div className="mt-10">
+      <LineChart
+        style={{
+          width: "100%",
+          maxWidth: "800px",
+          maxHeight: "80vh",
+          aspectRatio: 1.618,
+        }}
         responsive
         margin={{
-            top:20,
-            right:20,
-            bottom:5,
-            left:0
+          top: 0,
+          right: 10,
+          bottom: 0,
+          left: 0,
         }}
-        data={weeklySummary}>
-            
-            <Line dataKey='attendance' stroke='#0051ff'/>
-            <XAxis dataKey='date'/>
-            <YAxis/>
-        </LineChart>
-    );
-}
+        data={weeklySummary}
+      >
+        <Line
+          dataKey="attendance"
+          type="monotone"
+          stroke="#0051ff"
+          strokeWidth={2}
+          name="Church attendance"
+        />
+        <XAxis
+          stroke="#000000"
+          dataKey="date"
+          label={{ value: "Date", position: "bottom" }}
+        />
+        <YAxis
+          stroke="#000000"
+          width="auto"
+          label={{ value: "No. of People", position: "insideLeft", angle: -90 }}
+        />
+        <Tooltip />
+        <Legend
+          width={200}
+          wrapperStyle={{
+            top: -20,
+            right: -20,
+          }}
+        />
+      </LineChart>
+    </div>
+  );
+};
 export default AttendanceChart;
