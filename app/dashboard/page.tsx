5e7c58787ebf047ef0186ca791385ec231e07b23
SamGombs20 <gombajosh18@gmail.com>
2026-03-17 10:58:50 +0300

    Update:User dashboard design

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -48,7 +48,7 @@ const cardData = [
         ></path>
       </>
     ),
-    details: "30,000",
+    details: "Ksh.30,000",
   },
   {
     title: "Visitors",
@@ -74,7 +74,7 @@ const DashboardPage = () => {
         <div className="flex gap-3 flex-wrap justify-evenly lg:w-3xl lg:justify-between mt-8">
             {cardData.map((item, index) => (
           <div key={index} className="flex min-w-50 border border-blue-300 rounded-xl p-3 flex-col">
-            <div className="flex items-center">
+            <div className="flex text-blue-800 mb-1 items-center">
               <svg
                 className="w-5 h-5 mr-2"
                 fill="none"
