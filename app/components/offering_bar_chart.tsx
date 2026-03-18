d9e8b2f10740dc4ed6d5a29e2b57e521869e82ad
SamGombs20 <gombajosh18@gmail.com>
2026-03-18 10:54:06 +0300

    Update:User dashboard trend charts

--- app/components/offering_bar_chart.tsx
+++ app/components/offering_bar_chart.tsx
@@ -11,12 +11,7 @@ const OfferingsChart = () => {
       }}
       responsive
       data={weeklySummary}
-      margin={{
-        top: 5,
-        right: 0,
-        left: 0,
-        bottom: 5,
-      }}
+      
     >
       <CartesianGrid strokeDasharray="3 4" stroke="#f3f4f6" />
       <XAxis
@@ -34,9 +29,12 @@ const OfferingsChart = () => {
         dataKey="offerings"
         name="Offerings Collection"
         fill="#0051ff"
+        activeBar={{fill:'#ff8c00'}}
         radius={[10, 10, 0, 0]}
       />
-      <Tooltip />
+      <Tooltip
+      
+       />
       <Legend
         width={200}
         wrapperStyle={{
