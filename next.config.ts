39e94228a42b65520fdad6c387560a4396fed2ca
Samuel Gomba <91277415+SamGombs20@users.noreply.github.com>
2026-03-20 13:28:37 +0000

    Update:Cloud development

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: ["ntdkry-3000.csb.app"],
 };
 
 export default nextConfig;
