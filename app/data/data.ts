70b57a737854851a82b3c66ad0567b3efa9197c8
Samuel Gomba <91277415+SamGombs20@users.noreply.github.com>
2026-03-20 13:34:56 +0000

    Update:Cloud development working

--- app/data/data.ts
+++ app/data/data.ts
@@ -9,8 +9,8 @@ export const weeklySummary: DailySummary[] = [
   { week: 6, date: "2026-Feb-09", attendance: 198, offerings: 88000 },
   { week: 7, date: "2026-Feb-16", attendance: 215, offerings: 110500 },
   { week: 8, date: "2026-Feb-23", attendance: 230, offerings: 135000 },
-  { week: 9, date: "2026-Feb-02", attendance: 212, offerings: 102000 },
-  { week: 10, date: "2026-Feb-09", attendance: 190, offerings: 95000 },
-  { week: 11, date: "2026-Feb-16", attendance: 225, offerings: 125000 },
-  { week: 12, date: "2026-Feb-23", attendance: 208, offerings: 108000 },
+  { week: 9, date: "2026-Mar-02", attendance: 212, offerings: 102000 },
+  { week: 10, date: "2026-Mar-09", attendance: 190, offerings: 95000 },
+  { week: 11, date: "2026-Mar-16", attendance: 225, offerings: 125000 },
+  { week: 12, date: "2026-Mar-23", attendance: 208, offerings: 108000 },
 ];
