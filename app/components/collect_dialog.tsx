5c3bb6cef404cf0636d20e1bc42e13c43d176f16
Samuel Gomba <91277415+SamGombs20@users.noreply.github.com>
2026-03-23 12:59:11 +0000

    Update:Attendance proportion chart

--- app/components/collect_dialog.tsx
+++ app/components/collect_dialog.tsx
@@ -3,6 +3,18 @@ const CollectDataForm =()=>{
         <div>
             <form>
                 <div className="input-div">
+                    <input className="user-input" placeholder="No. of adults"/>
+                </div>
+                <div className="input-div">
+                    <input className="user-input" placeholder="No. of children" />
+                </div>
+                <div className="input-div">
+                    <input className="user-input" placeholder="Cash Collections"/>
+                </div>
+                <div className="input-div">
+                    <input className="user-input" placeholder="Bank Collections"/>
+                </div>
+                <div className="input-div">
                     <input className="user-input" placeholder=""/>
                 </div>
             </form>
