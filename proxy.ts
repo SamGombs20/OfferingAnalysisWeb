88ed3a67a404719bf1fded56412de149e23f2f24
SamGombs20 <gombajosh18@gmail.com>
2026-03-23 12:35:10 +0300

    Update:Set up in stackblitz

--- proxy.ts
+++ proxy.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export const middleware =(request:NextRequest)=>{
+export const proxy =(request:NextRequest)=>{
     const accessToken = request.cookies.get('session_token')?.value;
     if(!accessToken){
         return NextResponse.redirect(new URL('/auth', request.url))
