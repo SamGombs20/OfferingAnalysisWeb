2745b58e58660225d04d629306e6895e33f56761
Samuel Gomba <91277415+SamGombs20@users.noreply.github.com>
2026-03-23 10:18:12 +0000

    Update:Using codespaces

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   allowedDevOrigins: ["ntdkry-3000.csb.app"],
+  experimental:{
+    serverActions:{
+      allowedOrigins:[
+        'localhost:3000',
+        '*.app.github.dev',
+        '*.github.dev'
+      ]
+    }
+  }
 };
 
 export default nextConfig;
