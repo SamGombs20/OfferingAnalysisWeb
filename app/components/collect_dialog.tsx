9821892b9aa4ff1534838855ce44e61bc8493d01
Samuel Gomba <91277415+SamGombs20@users.noreply.github.com>
2026-03-23 13:48:20 +0000

    Update:Collection Form

--- app/components/collect_dialog.tsx
+++ app/components/collect_dialog.tsx
@@ -1,8 +1,12 @@
+'use client'
+import { useState } from "react";
+
 const CollectDataForm = () => {
+    const[slide, setSlide] = useState(0);
     return (
         <div className="lg:w-xl">
             <form>
-                <div>
+                {slide==0 &&(<div>
                     <p className="text-2xl text-center">Givings</p>
                     <div className="input-div">
                         <input type="number" placeholder="Tithes" className="user-input" />
@@ -28,9 +32,10 @@ const CollectDataForm = () => {
                     <div className="input-div">
                         <input type="number" className="user-input" placeholder="Bank Collections" />
                     </div>
-                </div>
+                </div>)}
 
-                <div>
+                {slide==1 && (
+                    <div>
                     <p className="text-2xl text-center font-[500]">Attendance</p>
                     <div className="input-div">
                         <input type="number" className="user-input" placeholder="Adults" />
@@ -50,6 +55,11 @@ const CollectDataForm = () => {
                         <input type="date" className="user-input" placeholder="Date" />
                     </div>
                 </div>
+                )}
+            <div className="flex">
+             <button className="transition-all duration-400 ease-in-out hover:bg-red-600 hover:text-white text-red-600 border rounded-xl py-1 px-4">{slide==0?"Close":"Back"}</button>
+             <button></button>
+            </div>
             </form>
         </div>
     )
