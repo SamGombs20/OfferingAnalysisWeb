3689954a057bbce361df437db36e6edeabf3b5cd
Samuel Gomba <91277415+SamGombs20@users.noreply.github.com>
2026-03-23 13:32:17 +0000

    Update:Collection Form

--- app/components/collect_dialog.tsx
+++ app/components/collect_dialog.tsx
@@ -1,44 +1,51 @@
-const CollectDataForm =()=>{
+const CollectDataForm = () => {
     return (
         <div className="lg:w-xl">
             <form>
-                <div className="input-div">
-                    <input type="number" placeholder="Tithes" className="user-input"/>
+                <div>
+                    <p className="text-2xl">Givings</p>
+                    <div className="input-div">
+                        <input type="number" placeholder="Tithes" className="user-input" />
+                    </div>
+                    <div className="input-div">
+                        <input type="number" placeholder="Offerings" className="user-input" />
+                    </div>
+                    <div className="input-div">
+                        <input type="number" placeholder="Sunday School" className="user-input" />
+                    </div>
+                    <div className="input-div">
+                        <input type="number" placeholder="Building" className="user-input" />
+                    </div>
+                    <div className="input-div">
+                        <input type="number" placeholder="Evangelism" className="user-input" />
+                    </div>
+                    <div className="input-div">
+                        <input type="number" placeholder="Others" className="user-input" />
+                    </div>
+                    <div className="input-div">
+                        <input type="number" className="user-input" placeholder="Cash Collections" />
+                    </div>
+                    <div className="input-div">
+                        <input type="number" className="user-input" placeholder="Bank Collections" />
+                    </div>
                 </div>
+
                 <div className="input-div">
-                    <input type="number" placeholder="Offerings" className="user-input"/>
-                </div>
-                <div className="input-div">
-                    <input type="number" placeholder="Sunday School" className="user-input"/>
-                </div>
-                <div className="input-div">
-                    <input type="number" placeholder="Building" className="user-input"/>
-                </div>
-                <div className="input-div">
-                    <input type="number" placeholder="Evangelism" className="user-input"/>
-                </div>
-                <div className="input-div">
-                    <input type="number" placeholder="Others" className="user-input"/>
-                </div>
-                <div className="input-div">
-                    <input type="number" className="user-input" placeholder="Cash Collections"/>
-                </div>
-                <div className="input-div">
-                    <input type="number" className="user-input" placeholder="Bank Collections"/>
-                </div>
-                <div className="input-div">
-                    <input type="number" className="user-input" placeholder="Adults"/>
+                    <input type="number" className="user-input" placeholder="Adults" />
                 </div>
                 <div className="input-div">
                     <input type="number" className="user-input" placeholder="Children" />
                 </div>
-                
-                
+
+
                 <div className="input-div">
                     <input type="number" className="user-input" placeholder="Visitors" />
                 </div>
                 <div className="input-div">
-                    <input type="date" className="user-input" placeholder="Date"/>
+                    <input type="number" className="user-input" placeholder="New Converts" />
+                </div>
+                <div className="input-div">
+                    <input type="date" className="user-input" placeholder="Date" />
                 </div>
             </form>
         </div>
