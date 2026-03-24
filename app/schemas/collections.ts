f0223730ec17aee491a4e6f281fcb9deadb434a7
Samuel Gomba <91277415+SamGombs20@users.noreply.github.com>
2026-03-24 14:30:18 +0000

    Update:Collection form development

--- app/schemas/collections.ts
+++ app/schemas/collections.ts
@@ -2,16 +2,16 @@ import z, { date } from "zod";
 
 export const WeeklyCollectionSchema = z.object({
     tithes:z.string().trim().min(1, "Tithes should be more!").regex(/^\d+$/, "Must be a number"),
-    offerings:z.string().trim().min(1, "Offerings should be more!").regex(/^\d+$/),
-    sundaySchool:z.string().trim().min(1, "Sunday School should have given more!"),
-    building:z.string(),
-    evangelism:z.string(),
-    others:z.string(),
-    cashCollections:z.string().trim().min(1, "The cash collections should be more!"),
-    bankCollections:z.string(),
-    adults:z.string().min(1, "Certainly there were more people!"),
-    children:z.string().min(1, "Certainly there were more children!"),
-    visitors:z.string(),
-    newConverts:z.string(),
-    date:z.string().min(1, "This is definately a wrong date!")
+    offerings:z.string().trim().min(1, "Offerings should be more!").regex(/^\d+$/, "Must be a number"),
+    sundaySchool:z.string().trim().min(1, "Sunday School should have given more!").regex(/^\d+$/, "Must be a number"),
+    building:z.string().regex(/^\d+$/, "Must be a number"),
+    evangelism:z.string().regex(/^\d+$/, "Must be a number"),
+    others:z.string().regex(/^\d+$/, "Must be a number"),
+    cashCollections:z.string().trim().min(1, "The cash collections should be more!").regex(/^\d+$/, "Must be a number"),
+    bankCollections:z.string().regex(/^\d+$/, "Must be a number"),
+    adults:z.string().min(1, "Certainly there were more people!").regex(/^\d+$/, "Must be a number"),
+    children:z.string().min(1, "Certainly there were more children!").regex(/^\d+$/, "Must be a number"),
+    visitors:z.string().regex(/^\d+$/, "Must be a number"),
+    newConverts:z.string().regex(/^\d+$/, "Must be a number"),
+    date:z.string().min(1, "This is definately a wrong date!").regex(/^\d+$/, "Must be a number")
 })
